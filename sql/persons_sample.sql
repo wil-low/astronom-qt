c5e986279341c496a05b200e74bc60150b2740cd
vmisyats <vmisyats@localhost>
2011-06-19 14:26:00 +0300

    Ivushkin's natal date added

--- sql/persons_sample.sql
+++ sql/persons_sample.sql
@@ -3,4 +3,6 @@ insert into TIME_LOCATION (NAME, DATE_TIME, TIMEZONE_OFFSET, LOCATION, LATITUDE,
 	('Wolfgang Amadeus Mozart', '1756-01-27T20:00:00', '+005200', 'SALZBURG, AU', '+0474800', '-0130200');
 insert into TIME_LOCATION (NAME, DATE_TIME, TIMEZONE_OFFSET, LOCATION, LATITUDE, LONGITUDE) values
 	('Vasyl', '1963-03-08T01:46:00', '+030000', 'Валуйки, Белгородская обл., Россия', '+0501300', '+0380500');
+insert into TIME_LOCATION (NAME, DATE_TIME, TIMEZONE_OFFSET, LOCATION, LATITUDE, LONGITUDE) values
+	('Андрей Ивушкин', '1980-08-25T19:30:00', '+030000', 'Луганск, Украина', '+0483300', '+0391900');
 commit;
